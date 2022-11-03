9210109af47400c5849bed5b85f3dca620c2e223
msurovceva <surovcevamarija@icloud.com>
2022-11-03 16:17:21 +0100

    test

---
+++ FizzBuzz/src/main/java/com/example/fizzbuzz/FizzBuzzApplication.java
@@ -0,0 +1,21 @@
+package com.example.fizzbuzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FizzBuzzApplication {
+
+    public static void main(String[] args) {
+        FizzBuzzResult fizzBuzzResult = new FizzBuzzResult();
+
+        for (int i = 1; i <= 100; i++) {
+            String number = fizzBuzzResult.fizzBuzz(i);
+
+            System.out.println(number);
+        }
+    }
+}
+
+
+

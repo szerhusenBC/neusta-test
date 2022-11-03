ef90d46b31f70f374724f068d2dc7b6bc1a75319
msurovceva <surovcevamarija@icloud.com>
2022-11-03 11:37:37 +0100

    test

---
+++ FizzBuzz/src/test/java/com/example/fizzbuzz/FizzBuzzApplicationTest.java
@@ -0,0 +1,16 @@
+package com.example.fizzbuzz;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FizzBuzzApplicationTest {
+
+
+   /* @Test
+    public void testMultipleOfThreeAndFivePrintsFizzBuzz() {
+        assertEquals("FizzBuzz", FizzBuzzApplication.class(int 15));
+    }*/
+}
+
+
